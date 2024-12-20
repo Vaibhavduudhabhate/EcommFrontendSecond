d92223d4e0dc0f31dca2652fa265ac7ba1fb3fe5
VAIBHAV DUDHABHATE <111244764+Vaibhavduudhabhate@users.noreply.github.com>
2024-12-20 17:11:05 +0530

    Update app.config.js

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,3 +1,5 @@
-const Service_url = `http://localhost:8080`;
+// const Service_url = `http://localhost:8080`;
+const Service_url = `https://ecommerceappserver-nz53.onrender.com`;
+
 
 export { Service_url };
